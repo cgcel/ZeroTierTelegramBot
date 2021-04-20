d2562cd4d4131ab39067f32034c03f4f16a812cf
elvin <cgc.elvin@gmail.com>
2021-04-20 14:51:14 +0800

    Code formatting.

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # author: elvin
 
@@ -9,4 +9,5 @@ from telegrambot.zerotiertgbot import *
 if __name__ == '__main__':
     schedule.every(REFRESH_SECONDS).seconds.do(check_per_min)
     threading.Thread(target=run_schedule, name="ScheduleThread").start()
+    # bot.polling()
     bot.infinity_polling()
